f4984d2dc2df343535ca20809e5fe71a56c35570
Divyansh Shukla <divyansh.professional@gmail.com>
2020-06-24 23:48:19 +0530

    version 2

--- JS/index.js
+++ JS/index.js
@@ -91,4 +91,19 @@ function progressFunction()
 	let scrolled = (winScroll/height)*100;
 	document.getElementById("myBar").style.width = scrolled + "%";
 }
- 
+
+/*smooth scroll*/
+$(document).ready(function()
+{
+	$('a').on('click',function(event){
+		if(this.hash !== "")
+		{
+			event.preventDefault();
+			let hash = this.hash;
+			$('html','body').animate({
+				scrollTop: $(hash).offset().top} , 800, function(){
+					window.location.hash = hash;
+			});
+		}
+	});
+});
